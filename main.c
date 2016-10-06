1e5410e1122dd46d93e4bbaf6a1c7e82c4104057
johnczek <janhrboticky@gmail.com>
2016-10-06 21:10:08 +0200

    [MODIFY] 0.80 Testovaci commit z linuxu

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 int main() {
 
-    int a = 58;
     int johnnyJeKokod = 52;
 
     printf ("%d", johnnyJeKokod);
 
     for (int i = 0; i <= johnnyJeKokod; i++) {
         printf("Johnny je kokod po %d.\n\n", i);
+        printf("Nejakej text v linuxu");
     }
 
     return 0;
