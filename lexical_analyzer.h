bbb4830f230e15f23a58bf3d022bbf6fca840083
David <dave.hel7@gmail.com>
2016-10-20 04:46:35 +0200

    Update lexical_analyzer.h

--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -1,3 +1,27 @@
+/**
+ * IFJ/IAL - Lexikální analyzátor
+ * Varianta:    b/2/I
+ * Subor:       lexical_analyzer.h
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
+
 #include "lexical_analyzer.c"
 
+/**
+ * Prototyp funkce
+ */
 void printKeyWordTable(); // Vytiskne obsah tabulky klíčových slov
+
+
+/**
+ * Jednotlivé stavy automatu
+ */
+typedef enum {
+
+
+};
