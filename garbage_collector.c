b208a8e29479557ae603c9f2d6522c76e6fa7788
Johnczek <janhrboticky@gmail.com>
2016-10-24 17:25:40 +0200

    [ADD]  0.2 Přidání souborů pro práci s pamětí a listem

---
+++ garbage_collector.c
@@ -0,0 +1,5 @@
+//
+// Created by Johnczek on 24.10.2016.
+//
+
+#include "garbage_collector.h"
---
+++ garbage_collector.h
@@ -0,0 +1,8 @@
+//
+// Created by Johnczek on 24.10.2016.
+//
+
+#ifndef IFJ_59_GARBAGE_COLLECTOR_H
+#define IFJ_59_GARBAGE_COLLECTOR_H
+
+#endif //IFJ_59_GARBAGE_COLLECTOR_H
