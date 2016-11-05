2dd7a8595143ec4de34611b981bae886ec72f276
Ehmeed <milanhruban@seznam.cz>
2016-11-05 13:46:40 +0100

    [MODIFY] 0.3 - úprava hlaviček

--- parser.h
+++ parser.h
@@ -1,6 +1,14 @@
-//
-
-//
+/**
+ * IFJ/IAL - Lexikální analyzátor
+ * Varianta:    b/2/I
+ * Soubor:      parser.h
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
 
 #ifndef IFJ_59_PARSER_H
 #define IFJ_59_PARSER_H
