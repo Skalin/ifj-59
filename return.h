8ae255a6755729e8cdf6ea49530e74d024cdb753
David <dave.hel7@gmail.com>
2016-11-05 22:14:35 +0100

    [MODIFY] 0.3 - přidány návratové hodnoty

---
+++ return.h
@@ -0,0 +1,24 @@
+/**
+ * IFJ/IAL - Lexikální analyzátor
+ * Varianta:    b/2/I
+ * Soubor:      return.c
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
+ 
+#ifndef RETURN
+#define RETURN
+
+/**
+ * Jestliže proběhne činnost interpretu bez chyb, vrací se návratová hodnota 0.
+ * Pokud dojde k chybě, vrací se návratová hodnota podle zadání. 
+ */
+typedef enum {
+
+} retError;
+
+#endif
