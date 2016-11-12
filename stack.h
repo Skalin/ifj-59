c67345badd9ded9cfd77ca915da8bd6eed18c041
David <dave.hel7@gmail.com>
2016-11-13 00:03:02 +0100

    to do

---
+++ stack.h
@@ -0,0 +1,11 @@
+/**
+ * IFJ/IAL - Lexikální analyzátor
+ * Varianta:    b/2/I
+ * Soubor:      stack.h
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
