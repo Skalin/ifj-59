cd69142f795e818e8145d22fc65a0d6b5f569b81
Johnczek <janhrboticky@gmail.com>
2016-11-13 12:41:34 +0100

    [MODIFY]0.3 Kompletace zásobníku

--- stack.c
+++ stack.c
@@ -1,5 +1,5 @@
 /**
- * IFJ/IAL - Lexikální analyzátor
+ * IFJ/IAL - Stack
  * Varianta:    b/2/I
  * Soubor:      stack.c
  * Tým:         59
@@ -9,3 +9,51 @@
  *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
  *              David Hél, xhelda00@stud.fit.vutbr.cz
  */
+#include "stack.h"
+
+tStack * stackInit ( tStack *stack ) {
+    //Inicializace polozek stacku
+    tStack *stack = plusMalloc(sizeof(tStack));
+    stack->data = plusMalloc(sizeof(void) * 30);
+    stack->allocated = 30;
+    stack->counter = 0;
+
+    return stack;
+}
+
+int stackEmpty ( const tStack* s ) {
+    return(s->counter == 0 ? 1 : 0); // Pokud je vrchol zasobniku mensi jak nula
+}
+
+int stackFull ( const tStack* s ) {
+    return(s->allocated < (s->counter+1) ? 1 : 0); // Pokud se stav zasobniku rovna max kapacite, vrati se 1
+}
+
+void stackPush ( tStack *stack, void *data ) {
+    // Pokud uz neni dostatek alokovane pameti, provede se realloc
+    if(stackFull(stack)) {
+        stack->data = plusRealloc(stack->data, (sizeof(void *) * (stack->allocated+30)));
+        stack->allocated += 30;
+    }
+    //Na vrchol zasobniku vlozime data
+    stack->counter++;
+    stack->data[stack->counter] = data;
+}
+
+void * stackTop (tStack *stack) {
+    //Pokud jsou v zasobniku data, vrat data na vrcholu
+    if(!stackEmpty(stack))
+        return(stack->data[stack->counter+1]);
+    //Jinak vrat null
+    else
+        return NULL;
+}
+
+void stackPop ( tStack *stack ) {
+    if (!stackEmpty(stack))
+        stack->counter--;
+}
+
+int stackSize (tStack *stack) {
+    return stack->counter;
+}
--- stack.h
+++ stack.h
@@ -1,5 +1,5 @@
 /**
- * IFJ/IAL - Lexikální analyzátor
+ * IFJ/IAL - Stack
  * Varianta:    b/2/I
  * Soubor:      stack.h
  * Tým:         59
@@ -9,3 +9,59 @@
  *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
  *              David Hél, xhelda00@stud.fit.vutbr.cz
  */
+
+#ifndef IFJ_59_STACK_H
+#define IFJ_59_STACK_H
+
+//Struktura zásobníku
+typedef struct {
+    void ** data;
+    int counter;
+    int allocated;
+} tStack;
+
+/*
+ * Inicializuje zásobník
+ * @param zásobník
+ * @return inicializovaný zásobník
+ */
+tStack * stackInit ( tStack *stack );
+/*
+ * Vrací 1/0 hodnotu jestli je zásobník prázdný
+ * @param zásobník
+ * @return true/false
+ */
+int stackEmpty (tStack *s );
+/*
+ * Vrací 1/0 hodnotu jestli je zásobník plný
+ * @param zásobník
+ * @return true/false
+ */
+int stackFull (tStack *s );
+/*
+ * Push dat na zásobník
+ * @param zásobník
+ * @param vkládaná data
+ * @return
+ */
+void stackPush ( tStack *stack, void *data);
+/*
+ * Funkce navrátí ukazatel na vrchol zásobníku
+ * @param zásobník
+ * @return ukazatel na vrchol zasobniku
+ */
+void * stackTop (tStack *stack);
+/*
+ * Funkce popne data z vrcholu zásobníku
+ * @param zásobník
+ * @return
+ */
+void stackPop ( tStack *stack );
+/*
+ * Funkce navrátí hodnotu velikosti zásobníku
+ * @param zásobník
+ * @return velikost zásobníku
+ */
+int stackSize (tStack *stack);
+
+#endif
