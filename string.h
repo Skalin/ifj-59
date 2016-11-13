8046bd117c649d083e5c1023eeccff535ed45140
David <dave.hel7@gmail.com>
2016-11-14 00:10:54 +0100

    TODO (přidán základ kostry)

--- string.h
+++ string.h
@@ -1,5 +1,5 @@
 /**
- * IFJ/IAL - Lexikální analyzátor
+ * IFJ/IAL - Nekonečně dlouhé řetězce
  * Varianta:    b/2/I
  * Soubor:      string.h
  * Tým:         59
@@ -9,3 +9,27 @@
  *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
  *              David Hél, xhelda00@stud.fit.vutbr.cz
  */
+
+
+#ifndef STRING
+#define STRING
+
+#include <string.h>
+
+typedef struct SStr{
+  char *data;		   
+  int length;		        // délka řetězce
+  int allocatedSize;	  // alokovaná paměť
+} SString;
+
+SString * initString ();
+
+//FUNKCE VIZ ZADÁNÍ STR 9!!!!!!!!!!!!!!!!!!!!
+
+int addCharacter (SStr *str, char c);
+int writeString (SStr *str, char *str2);
+int strCmpString(SSt *str1, SString *str2);
+int strAddStr(string *str1, string *str2);
+void destroyString (SString *str);
+
+#endif
