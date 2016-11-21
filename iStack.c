1e07a302de6aceb99188cd9f9d7a18babb55eddb
David <dave.hel7@gmail.com>
2016-11-21 19:39:17 +0100

    vytvoření stacku pro instrukce

---
+++ iStack.c
@@ -0,0 +1,11 @@
+/**
+ * IFJ/IAL - Stack pro instrukce
+ * Varianta:    b/2/I
+ * Soubor:      iStack.c
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
