5b8f5259ecabe5a4ae3fb0752301179b871834bf
Dominik Skála <DominikSkala@seznam.cz>
2016-11-24 16:02:41 +0100

    [MODIFY] 0.5 - přesun struktur z interpretu a iStacku do typedef.h

--- typedef.h
+++ typedef.h
@@ -147,4 +147,45 @@ typedef enum {
     expAssign
 } tExpType;
 
+typedef enum {
+    // BUILT-IN FUNCTIONS
+    insIfj16readInt,
+    insIfj16readDouble,
+    insIfj16readString,
+    insIfj16lenght,
+    insIfj16substr,
+    insIfj16compare,
+    insIfj16find,
+    insIfj16sort,
+    insIfj16print,
+    //
+    // MATH
+    insPlus, // Sčítání
+    insMinus, // Odečítání
+    insMux, // Násobení
+    insDiv, // Dělení
+    //
+    //COMPARE
+    insEqual, // Je rovno
+    insNotEqual, // Neni rovno
+    insLess, // Je mensi
+    insLessOrEqual, // Je mensi nebo rovno
+    insGreater, // Je vetsi
+    insGreaterOrEqual, // Je vetsi nebo rovno
+    insAssignment
+}InstrType;
+
+ typedef struct Instr {
+     BTSNode *Id1; // Adresa prvního operandu
+     BTSNode *Id2; // Adresa druhého operandu
+     BTSNode *Id3; // Adresa, kam se uloží výsledek operace
+     InstrType type;
+ }Instr;
+
+typedef struct {
+    Instr *dataInstr;
+    int count;
+    int alloc;
+}instrStack;
+
 #endif
