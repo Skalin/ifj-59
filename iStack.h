d093a3a54e82ea3d22e6e67825b54c23d100c5dd
Dominik Skála <DominikSkala@seznam.cz>
2016-11-24 16:03:24 +0100

    [MODIFY] 0.5 - přesun struktur z interpretu a iStacku do typedef.h

--- iStack.h
+++ iStack.h
@@ -12,48 +12,7 @@
 
 #ifndef ISTACK_H
 #define ISTACK_H
-
-typedef enum {
-    // BUILT-IN FUNCTIONS
-    insIfj16readInt,
-    insIfj16readDouble,
-    insIfj16readString,
-    insIfj16lenght,
-    insIfj16substr,
-    insIfj16compare,
-    insIfj16find,
-    insIfj16sort,
-    insIfj16print,
-    //
-    // MATH
-    insPlus, // Sčítání
-    insMinus, // Odečítání
-    insMux, // Násobení
-    insDiv, // Dělení
-    //
-    //COMPARE
-    insEqual, // Je rovno
-    insNotEqual, // Neni rovno
-    insLess, // Je mensi
-    insLessOrEqual, // Je mensi nebo rovno
-    insGreater, // Je vetsi
-    insGreaterOrEqual, // Je vetsi nebo rovno
-    insAssignment
-}InstrType;
-
- typedef struct Instr {
-     BTSNode *Id1; // Adresa prvního operandu
-     BTSNode *Id2; // Adresa druhého operandu
-     BTSNode *Id3; // Adresa, kam se uloží výsledek operace
-     InstrType type;
- }Instr;
-
-typedef struct {
-    Instr *dataInstr;
-    int count;
-    int alloc;
-}instrStack;
-
+// přesunuty struktury do typedef.h
 
 //FUNKCE
 
