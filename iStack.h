f79a4b84a217edeba32d3df13a9d4dc1d8949be6
Dominik Skála <DominikSkala@seznam.cz>
2016-11-26 17:21:55 +0100

    Update iStack.h

--- iStack.h
+++ iStack.h
@@ -14,6 +14,49 @@
 #define ISTACK_H
 #include "garbage_collector.h"
 
+
+typedef enum {
+	// BUILT-IN FUNCTIONS
+	insIfj16readInt,
+	insIfj16readDouble,
+	insIfj16readString,
+	insIfj16lenght,
+	insIfj16substr,
+	insIfj16compare,
+	insIfj16find,
+	insIfj16sort,
+	insIfj16print,
+	//
+	// MATH
+	insPlus, // Sčítání
+	insMinus, // Odečítání
+	insMux, // Násobení
+	insDiv, // Dělení
+	//
+	//COMPARE
+	insEqual, // Je rovno
+	insNotEqual, // Neni rovno
+	insLess, // Je mensi
+	insLessOrEqual, // Je mensi nebo rovno
+	insGreater, // Je vetsi
+	insGreaterOrEqual, // Je vetsi nebo rovno
+	insAssignment,
+    insFunctionCall
+}InstrType;
+
+typedef struct Instr {
+	BTSNode *Id1; // Adresa prvního operandu
+	BTSNode *Id2; // Adresa druhého operandu
+	BTSNode *Id3; // Adresa, kam se uloží výsledek operace
+	InstrType type;
+}Instr;
+
+typedef struct {
+	Instr **dataInstr;
+	int count;
+	int alloc;
+}instrStack;
+
 //FUNKCE
 instrStack* instrStackInit (instrStack* stc );
 Instr *instrItemInit (Instr* stc);
