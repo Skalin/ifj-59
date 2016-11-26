7720b2fd6e614fe5db33aae703cd3ba4d187ed19
Dominik Skála <DominikSkala@seznam.cz>
2016-11-26 17:21:47 +0100

    Update typedef.h

--- typedef.h
+++ typedef.h
@@ -223,47 +223,4 @@ typedef struct {
 	BTSNode *actFunction; // Jaká funkce je aktivní, resp. v jaké funkci se nacházíme
 } mainTree;
 
-
-typedef enum {
-	// BUILT-IN FUNCTIONS
-	insIfj16readInt,
-	insIfj16readDouble,
-	insIfj16readString,
-	insIfj16lenght,
-	insIfj16substr,
-	insIfj16compare,
-	insIfj16find,
-	insIfj16sort,
-	insIfj16print,
-	//
-	// MATH
-	insPlus, // Sčítání
-	insMinus, // Odečítání
-	insMux, // Násobení
-	insDiv, // Dělení
-	//
-	//COMPARE
-	insEqual, // Je rovno
-	insNotEqual, // Neni rovno
-	insLess, // Je mensi
-	insLessOrEqual, // Je mensi nebo rovno
-	insGreater, // Je vetsi
-	insGreaterOrEqual, // Je vetsi nebo rovno
-	insAssignment,
-    insFunctionCall
-}InstrType;
-
-typedef struct Instr {
-	BTSNode *Id1; // Adresa prvního operandu
-	BTSNode *Id2; // Adresa druhého operandu
-	BTSNode *Id3; // Adresa, kam se uloží výsledek operace
-	InstrType type;
-}Instr;
-
-typedef struct {
-	Instr **dataInstr;
-	int count;
-	int alloc;
-}instrStack;
-
 #endif
