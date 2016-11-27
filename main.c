e1381502371040454fa3047b4fda1d50fb88495d
Dominik Skála <dominikskala@seznam.cz>
2016-11-27 13:02:15 +0100

    [MODIFY] 0.6. - uprava TAB a mezer

--- main.c
+++ main.c
@@ -9,11 +9,11 @@
 
 
 static const char *tokenTypeString[] = {
-    "t_error","t_int","t_double","t_double_e","t_string","t_simple_ident","t_complete_ident","t_div","t_multi","t_plus","t_minus","t_greater","t_greater_eq","t_less","t_less_eq","t_assignment","t_comparasion","t_excl_mark","t_comparasion_ne","t_sq_bracket_l","t_sq_bracket_r","t_bracket_l","t_bracket_r","t_brace_l","t_brace_r","t_simple_comment","t_block_comment_start","t_block_comment_end","t_semicolon","t_comma","t_eof","t_kw_class","t_kw_static","t_kw_boolean","t_kw_double","t_kw_int","t_kw_string","t_kw_void","t_kw_while","t_kw_for","t_kw_break","t_kw_return","t_kw_continue","t_kw_do","t_kw_if","t_kw_else","t_kw_true","t_kw_false"
+	"t_error","t_int","t_double","t_double_e","t_string","t_simple_ident","t_complete_ident","t_div","t_multi","t_plus","t_minus","t_greater","t_greater_eq","t_less","t_less_eq","t_assignment","t_comparasion","t_excl_mark","t_comparasion_ne","t_sq_bracket_l","t_sq_bracket_r","t_bracket_l","t_bracket_r","t_brace_l","t_brace_r","t_simple_comment","t_block_comment_start","t_block_comment_end","t_semicolon","t_comma","t_eof","t_kw_class","t_kw_static","t_kw_boolean","t_kw_double","t_kw_int","t_kw_string","t_kw_void","t_kw_while","t_kw_for","t_kw_break","t_kw_return","t_kw_continue","t_kw_do","t_kw_if","t_kw_else","t_kw_true","t_kw_false"
 };
 
 void printToken(tToken *token) {
-    printf("%20s %25s\n",token->data,tokenTypeString[token->type]);
+	printf("%20s %25s\n",token->data,tokenTypeString[token->type]);
 }
 
 int main(int argc, char* argv[]) {
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
 	} else {
 		globalInit();
 		global.file = fopen("testing/fac_iter.ifj", "r");
-		if ( global.file == NULL)
+		if (global.file == NULL)
 		//    throwException(99, 0, 0);
 		global.fileName = argv[1];
 		initTree(global.mTree);
