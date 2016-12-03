22679ffd12d7aab11f1ef654ee728177bf6fd98e
Dominik Skála <dominikskala@seznam.cz>
2016-12-03 16:35:04 +0100

    [MODIFY] 0.6 - syntax testy a test pro sort

---
+++ testing/tests.c
@@ -0,0 +1,23 @@
+#include "../main.h"
+
+int main (int argc, char *argv) {
+
+	String testString = "kokot";
+	String testString2 = "vysortovat";
+	String testString3 = "posun";
+	String testString4 = "boyle-moore";
+
+	sort(testString);
+	print(testString);
+
+	sort(testString2);
+	print(testString2);
+
+	sort(testString3);
+	print(testString3);
+
+	sort(testString4);
+	print(testString4);
+
+
+}
