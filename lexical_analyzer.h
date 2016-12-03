1e0042e57a9ddd5453b510a1091cbc141a66e37f
Dominik Skála <dominikskala@seznam.cz>
2016-12-03 17:23:19 +0100

    [MODIFY] 0.6 - uprava printToken

--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -21,11 +21,6 @@
 #define NUMBER_OF_KEY_WORDS 17
 #define NUMBER_OF_RESERVED_WORDS
 
-// pole stavu pro debug lexu
-static const char *tokenTypeString[] = {
-		"t_bracket_l","t_bracket_r","t_div","t_multi","t_plus","t_minus","t_comparasion","t_comparasion_ne","t_greater","t_less","t_less_eq","t_greater_eq","t_excl_mark","t_semicolon","t_simple_ident","t_complete_ident","t_int","t_double","t_double_e","t_string","t_error","t_assignment","t_brace_l","t_brace_r","t_sq_bracket_l","t_sq_bracket_r","t_simple_comment","t_block_comment_start","t_block_comment_end","t_eof","t_comma","t_kw_class","t_kw_static","t_kw_boolean","t_kw_double","t_kw_int","t_kw_string","t_kw_void","t_kw_while","t_kw_for","t_kw_break","t_kw_return","t_kw_continue","t_kw_do","t_kw_if","t_kw_else","t_kw_true","t_kw_false","t_find","t_sort","t_length","t_substring","t_compare","t_read_int","t_read_double","t_read_string","t_print"
-};
-
 /*
  * Jednotlivé stavy automatu
  */
