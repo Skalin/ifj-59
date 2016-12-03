ad431f2e7cc33757ae4f3729911c4bece9128a61
HEKTOR88 <hons46@gmail.com>
2016-12-03 13:09:48 +0100

    0.6 prototypy funkci

--- expressions.h
+++ expressions.h
@@ -17,6 +17,47 @@
 #include "typedef.h"
 
 
+/**
+ * IFJ/IAL - Precedenční analýza
+ * Varianta:    b/2/I
+ * Soubor:      expressions.c
+ * Tým:         59
+ * Autoři:      Jan Hrbotický, xhrbot01@stud.fit.vutbr.cz
+ *              Dominik Skála, xskala11@stud.fit.vutbr.cz
+ *              Milan Hruban, xhruba08@stud.fit.vutbr.cz
+ *              Martin Hons, xhonsm00@stud.fit.vutbr.cz
+ *              David Hél, xhelda00@stud.fit.vutbr.cz
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include "garbage_collector.h"
+#include "expressions.h"
+#include "error_handler.h"
+#include "stack.h"
+#include "ial.h"
+#include "instrstack.h"
+#include "lexical_analyzer.h"
+
+#define STR_ERROR   0
+#define STR_SUCCESS 1
+#define STR_ALLOCATION_SIZE 8  // Udává, kolik bude alokováno na začátku paměti. Pokud načítáme po jednom znaku, dojde k alokaci na násobky tohoto čísla
+
+
+bool isIdent(tToken *token);
+bool isConst(tToken *token);
+char getPrecChar(tToken *stackToken, tToken *inToken);
+tStackIt **chnToExp(tStack *stack, tStackIt *handle[]);
+void reduceExp(BTSNode *targetId, tStackIt *handle[3], instrStack *iStack);
+void expression(BTSNode *targetId, tExpType expType);
+void completeInstr();
+int strEqual(String str1, String str2);
+
+
+
 /*
  *
  */
