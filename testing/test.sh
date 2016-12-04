de59357e71be96c4bb8af16b5b4f79c314542f52
Dominik Skála <dominikskala@seznam.cz>
2016-12-04 22:56:56 +0100

    [MODIFY] 0.7 - uprava testu, pracuji na bash scriptu pro test

--- testing/test.sh
+++ testing/test.sh
@@ -6,9 +6,26 @@ if [ $# -lt 1 ]; then
 fi
 
 
-if [ "$1" == "./ifj16" ];then
+if [ "$1" != "./ifj16" ];then
     echo "Je treba spustit program nad prekladacem ifj16!";
     exit 1
 fi
+
+runtest=$1
+
+extension=".ifj16"
+semstring="sem"
+semtests=1
+while [ $semtests -le 46 ]; do
+    semtest=$sem$semtests
+    semtest="$runtest $semtest$extension"
+    $semttests+1
+
+    $semtest;
+
+
+    printf $?;
+
+done
 
 exit 0
