54fbaab3b224fc44a9a6a7c8368e216a938073ff
Dominik Skála <DominikSkala@seznam.cz>
2016-12-09 11:33:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,52 +10,32 @@ mainTree mTree;
 
 int main(int argc, char* argv[]) {
 	// Pokud se program spousti se spatnym poctem parametru, ukonci se
-	if(argc < 2) {
+	if(argc != 2) {
 		throwException(99, 0, 0);
 	} else {
- 		if (argc > 3) {
- 			throwException(99, 0, 0);
- 		} else if (argc == 3) {
-			if (strcmp(argv[2], "NONDEBUG") == 0) {
-			// Inicializace globalni promenne v pripade nedebug verze aplikace
-				globalInit();
-			} else if (strcmp(argv[2], "DEBUG") == 0) {
-			// Inicializace globalni promenne v pripade debug verze aplikace
-				globalInit();
-				global.DEBUG = 1;
-			} else {
-				// navrat chyby ve stavu kdy jako treti parameter dorazi blbost
-				throwException(99, 0, 0);
-			}
-		} else {
-			// nic
+		globalInit();
+
+
+		// Otevreni souboru
+		//printf("%s", argv[1]);
+		global.file = fopen(global.fileName = argv[1], "r");
+		if (global.file == NULL) {
+			throwException(99, 0, 0);
 		}
 
-		//printf("dostal jsem se az k podmince mezi testem a normalnim prekladacem");
+		printf("Jsem pred initem stromu\n");
+		initTree(&mTree);
 
-		if (global.DEBUG == 0) { // normalni funkce programu
-			
-			globalInit();
-			printf("Jsem pred initem\n");
-			initTree(&mTree);
+		mTree.root = plusMalloc(sizeof(BTSNode));
+		mTree.root = NULL;
+		mTree.actFunction = plusMalloc(sizeof(BTSNode));
+		mTree.actFunction = NULL;
+		mTree.actClass = plusMalloc(sizeof(BTSNode));
+		mTree.actClass = NULL;
+		// globalni init
 
-			mTree.root = plusMalloc(sizeof(BTSNode));
-			mTree.root = NULL;
-			mTree.actFunction = plusMalloc(sizeof(BTSNode));
-			mTree.actFunction = NULL;
-			mTree.actClass = plusMalloc(sizeof(BTSNode));
-			mTree.actClass = NULL;
-			// globalni init
-			globalInit();
-			// Otevreni souboru
-			//printf("%s", argv[1]);
-			global.file = fopen(global.fileName = argv[1], "r");
-			if (global.file == NULL) {
-				throwException(99, 0, 0);
-			}
+		printf("\n");
 
-			printf("\n");
-    
             BTSNode *pokusnode = createNewNode("trida",class,var_null,1,1);
             pokusnode = createNewNode("funkce",function,var_int,0,1);
             pokusnode = createNewNode("promena",var,var_int,0,1);
@@ -95,124 +75,6 @@ int main(int argc, char* argv[]) {
 
 			// Uvolneni pameti a ukonceni programu
 			finalFree();
-		} else {
-			// DEBUG aplikace s testy vestavenych funkci
-/*
-			// test stringu a jejich sortovani
-			char testString[] = "ultrasupermegasupersonicstring";
-			char testString2[] = "vysortovat";
-			char testString3[] = "posun";
-			char testString4[] = "boyle-moore";
-			char testString5[] = "Barbora";
-			// test vyhledavani
-			char hledej[] = "onic";
-			char hledej1[] = "bor";
-			char hledej2[] = "vat";
-			char hledej3[] = "n";
-			char hledej4[] = "\0";
-			char hledej5[] = "ajfkaojfkaljflkaafafaafa";
-
-			int where = 0;
-
-			where = find(testString5, hledej1);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej1, testString5, where);
-			where = find(testString, hledej);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej, testString, where);
-			where = find(testString2, hledej2);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej2, testString2, where);
-			where = find(testString3, hledej3);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej3, testString3, where);
-			where = find(testString4, hledej4);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej4, testString4, where);
-			where = find(testString, hledej5);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej5, testString, where);
-
-
-			char *printedString = testString;
-			printf("Originalni string \"%s\" ", testString);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString2;
-			printf("Originalni string \"%s\" ", testString2);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString3;
-			printf("Originalni string \"%s\" ", testString3);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString4;
-			printf("Originalni string \"%s\" ", testString4);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString5;
-			printf("Originalni string \"%s\" ", testString5);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-
-
-			where = find(testString5, hledej1);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej1, testString5, where);
-			where = find(testString, hledej);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej, testString, where);
-			where = find(testString2, hledej2);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej2, testString2, where);
-			where = find(testString3, hledej3);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej3, testString3, where);
-			where = find(testString4, hledej4);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej4, testString4, where);
-			where = find(testString, hledej5);
-			printf("Substring \"%s\" byl nalezen ve slove \"%s\" na pozici: %d\n", hledej5, testString, where);
-
-
-			printedString = testString;
-			printf("Originalni string \"%s\" ", testString);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString2;
-			printf("Originalni string \"%s\" ", testString2);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString3;
-			printf("Originalni string \"%s\" ", testString3);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString4;
-			printf("Originalni string \"%s\" ", testString4);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);
-			printedString = testString5;
-			printf("Originalni string \"%s\" ", testString5);
-			printedString = sort(printedString);
-			printf("Vysortovany string \"%s\"\n", printedString);*/
-			fprintf(stderr, "==========================================================================\n" );
-			fprintf(stderr, "========================== PROBEHNE HLAVNI TEST ==========================\n" );
-			fprintf(stderr, "======================= TEST JEDNOTLIVYCH SOUBORU! =======================\n\n" );
-
-
-			globalInit();
-			//printf("dostal jsem se za globalinit");
-			// Otevreni souboru
-			//printf("%s", argv[1]);
-			global.file = fopen(global.fileName = argv[1], "r");
-			if (global.file == NULL) {
-				throwException(99, 0, 0);
-			}
-
-
-			// Parser
-			pParse();
-            
-        
-			// Interpret
-			semCheck(global.iStack);
-			interpretMainCore(global.iStack);
-
-
-			// Uvolneni pameti a ukonceni programu
-			finalFree();
-
-			return 0;
-		}
 	}
 }
 
